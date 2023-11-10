79616e9b23e4392bb0314af3c139a9a7c15b9afd
Carlo Minucci <gecco@napodano.com>
2023-11-10 12:15:02 +0100

    Un po' di bugfix vari che manco mi ricordo più

--- install-itchbox.sh
+++ install-itchbox.sh
@@ -6,7 +6,7 @@ mkdir itchbox
 mkdir itchbox/data
 mkdir itchbox/download
 mkdir itchbox/games
-echo "*****-----> Scarico e installo wget..."
+echo "*****-----> Scarico e installo curl..."
 sudo apt -y install curl
 echo "*****-----> Sarico i sorgenti di joymap..."
 curl -O https://raw.githubusercontent.com/wizlab-it/joymap/master/joymap.c 
@@ -17,26 +17,29 @@ echo "*****-----> Compilo joypad e rimuovo i sorgenti..."
 gcc joymap.c -lxdo -o itchbox/data/joymap
 rm joymap.c
 echo "*****-----> Scarico le altre parti del core system..."
-curl --output itchbox/estrai.sh --url https://minucci.net/itchbox/estrai.sh
-curl --output itchbox/spegni.sh --url https://minucci.net/itchbox/spegni.sh
-curl --output itchbox/data/sfondo.jpg --url https://minucci.net/itchbox/sfondo.jpg
+curl --output itchbox/estrai.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/estrai.sh
+curl --output itchbox/spegni.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main//spegni.sh
+curl --output itchbox/data/sfondo.jpg --url https://raw.githubusercontent.com/carlominucci/itchbox/main/sfondo.jpg
+curl --output itchbox/data/itchbox128.png --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox128.png
+curl --output itchbox/joypadconf.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/joypadconf.sh
+chmod 775 itchbox/joypadconf.sh
 echo "*****-----> Setto i permessi del core system..."
 chmod 775 itchbox/estrai.sh
 chmod 775 itchbox/spegni.sh
 echo "*****-----> Scarico e installo i comandi del core system..."
-curl --output Desktop/ --url https://minucci.net/itchbox/Aggiorna.desktop
-curl --output Desktop/ --url https://minucci.net/itchbox/Spegni.desktop
+curl --output Desktop/Aggiorna.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Aggiorna.desktop 
+curl --output Desktop/Spegni.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Spegni.desktop
 chmod 775 Desktop/Aggiorna.desktop
 chmod 755 Desktop/Spegni.desktop
 echo "*****-----> Configuro l'avvio automatico di itchbox..."
-mkdir .config/autostart
-curl --output .config/autostart/itchbox.desktop --url https://minucci.net/itchbox/itchbox.desktop
-chmod 755 ./confif/autostart/itchbox.desktop
+mkdir .config/autostart/
+curl --output .config/autostart/itchbox.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox.desktop
+chmod 755 ./config/autostart/itchbox.desktop
 echo "*****-----> Configuro la condivisione di rete per l'upload dei pacchetti dei giochi..."
 sudo apt -y install samba
 sudo apt -y install samba-common
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
-sudo --output /etc/samba/smb.conf --url https://minucci.net/itchbox/smb.conf
+sudo curl --output /etc/samba/smb.conf --url https://raw.githubusercontent.com/carlominucci/itchbox/main/smb.conf
 echo "*****-----> Inserisci la password per la cartella di rete..."
 sudo /etc/init.d/smbd restart
 echo "*****-----> Installazione completata..."
