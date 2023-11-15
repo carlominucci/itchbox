d71c6f3508b1502410798fb2d405104bef53d7b1
Carlo Minucci <gecco@napodano.com>
2023-11-15 11:28:09 +0100

    C'erano un po' di path sbagliati

--- install-itchbox.sh
+++ install-itchbox.sh
@@ -1,45 +1,71 @@
 #!/bin/bash
+clear
 echo "*****-----> Installo il core system di itchbox..."
 cd
+echo
 echo "*****----->Creo le cartelle di sistema..."
-mkdir itchbox
-mkdir itchbox/data
-mkdir itchbox/download
-mkdir itchbox/games
+echo
+mkdir $HOME/itchbox
+mkdir $HOME/itchbox/data
+mkdir $HOME/itchbox/download
+mkdir $HOME/itchbox/games
+echo
 echo "*****-----> Scarico e installo curl..."
+echo
 sudo apt -y install curl
+echo
 echo "*****-----> Sarico i sorgenti di joymap..."
+echo
 curl -O https://raw.githubusercontent.com/wizlab-it/joymap/master/joymap.c 
+echo
 echo "*****-----> Installo le dipendenze e il compilatore..."
+echo
 sudo apt -y install libxdo-dev
 sudo apt -y install gcc
+echo
 echo "*****-----> Compilo joypad e rimuovo i sorgenti..."
-gcc joymap.c -lxdo -o itchbox/data/joymap
+echo
+gcc joymap.c -lxdo -o $HOME/itchbox/data/joymap
 rm joymap.c
+echo
 echo "*****-----> Scarico le altre parti del core system..."
-curl --output itchbox/estrai.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/estrai.sh
-curl --output itchbox/spegni.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main//spegni.sh
-curl --output itchbox/data/sfondo.jpg --url https://raw.githubusercontent.com/carlominucci/itchbox/main/sfondo.jpg
-curl --output itchbox/data/itchbox128.png --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox128.png
-curl --output itchbox/joypadconf.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/joypadconf.sh
-chmod 775 itchbox/joypadconf.sh
+echo
+curl --output $HOME/itchbox/estrai.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/estrai.sh
+curl --output $HOME/itchbox/spegni.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main//spegni.sh
+curl --output $HOME/itchbox/data/sfondo.jpg --url https://raw.githubusercontent.com/carlominucci/itchbox/main/sfondo.jpg
+curl --output $HOME/itchbox/data/itchbox128.png --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox128.png
+curl --output $HOME/itchbox/joypadconf.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/joypadconf.sh
+chmod 775 $HOME/itchbox/joypadconf.sh
+echo
 echo "*****-----> Setto i permessi del core system..."
-chmod 775 itchbox/estrai.sh
-chmod 775 itchbox/spegni.sh
+echo
+chmod 775 $HOME/itchbox/estrai.sh
+chmod 775 $HOME/itchbox/spegni.sh
+echo
 echo "*****-----> Scarico e installo i comandi del core system..."
-curl --output Desktop/Aggiorna.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Aggiorna.desktop 
-curl --output Desktop/Spegni.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Spegni.desktop
-chmod 775 Desktop/Aggiorna.desktop
-chmod 755 Desktop/Spegni.desktop
+echo
+DESKTOPPATH=$(cat $HOME/.config/user-dirs.dirs | grep DESKTOP | awk -F "/" {'print $2'} | sed -e 's/"//g')
+curl --output $DESKTOPPATH/Aggiorna.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Aggiorna.desktop 
+curl --output $DESKTOPPATH/Spegni.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Spegni.desktop
+chmod 775 $DESKTOPPATH/Aggiorna.desktop
+chmod 755 $DESKTOPPATH/Spegni.desktop
+echo
 echo "*****-----> Configuro l'avvio automatico di itchbox..."
-mkdir .config/autostart/
-curl --output .config/autostart/itchbox.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox.desktop
-chmod 755 ./config/autostart/itchbox.desktop
+echo
+mkdir $HOME/.config/autostart/
+curl --output $HOME/.config/autostart/itchbox.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox.desktop
+chmod 755 $HOME/.config/autostart/itchbox.desktop
+echo
 echo "*****-----> Configuro la condivisione di rete per l'upload dei pacchetti dei giochi..."
+echo
 sudo apt -y install samba
 sudo apt -y install samba-common
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
 sudo curl --output /etc/samba/smb.conf --url https://raw.githubusercontent.com/carlominucci/itchbox/main/smb.conf
+echo
 echo "*****-----> Inserisci la password per la cartella di rete..."
+echo
+sudo smbpasswd -a itchbox
 sudo /etc/init.d/smbd restart
+echo
 echo "*****-----> Installazione completata..."
