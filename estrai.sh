3e38897bf486cb76a4ac3da6e5d6f189e13cb30f
Carlo Minucci <gecco@napodano.com>
2023-11-22 13:02:22 +0100

    Corrette alcune colorazioni e delle quisquiglie varie non degne di nota

--- estrai.sh
+++ estrai.sh
@@ -8,35 +8,34 @@ FORMATO=$(file --mime-type "download/$NOME" | awk -F ":" '{print $2}' | awk -F "
 
 if [[ -z $NOME ]]
 then 
-    echo "Nessun nuovo gioco in download"
+    echo -e "\033[0;31mNessun nuovo gioco in download"
     exit 1
 fi
 
-echo $NOME
-echo $FORMATO
+echo -e "\033[0;32mTrovato un nuovo gioco: \033[0;37m$NOME"
 
 if [[ $FORMATO == "zip" ]]
 then
-    echo "Scompatto il pacchetto .zip"
+    echo -e "\033[0;33mScompatto il pacchetto .zip"
     unzip -o "download/$NOME" -d "games/$NOMEDIR"
 elif [[ $FORMATO == "gzip" ]]
 then
-    echo "Scompatto il pacchetto .tar.gz"
-    mkdir "games/$NOMEDIR"
+    echo -e "\033[0;34mScompatto il pacchetto .tar.gz"
+    mkdir -v "games/$NOMEDIR"
     tar -zxvf "download/$NOME" -C "games/$NOMEDIR"
 elif [[ $FORMATO == "x-bzip2" ]]
 then
-    echo "Scompatto il pacchetto .bzip2"
-    mkdir "games/$NOMEDIR"
+    echo -e "\033[0;35mScompatto il pacchetto .bzip2"
+    mkdir -v "games/$NOMEDIR"
     tar -xvjf "download/$NOME" -C "games/$NOMEDIR"
 elif [[ $FORMATO == "rar" ]]
 then
-    echo "Scompatto il pacchetto .rar"
-    mkdir "games/$NOMEDIR"
+    echo -e "\033[0;36mScompatto il pacchetto .rar"
+    mkdir -v "games/$NOMEDIR"
     unrar "download$NOME" "games/$NOMEDIR"
 fi
 
-rm "download/$NOME"
+rm -v "download/$NOME"
 
 if [[ $(file "games/$NOMEDIR"/* | grep -v directory | wc -l) = 0 ]]
 then
@@ -48,48 +47,45 @@ else
     PATHGIOCO="games/$NOMEDIR"
 fi
 
-echo $PATHGIOCO
+echo -e "\033[0;32mDetermino il tipo di eseguibile: \033[0;37m$PATHGIOCO"
 
 if [[ $(file --mime-type "$PATHGIOCO"/* | grep "text/x-shellscript") ]]
 then
     ESEGUIBILE=`file --mime-type "$PATHGIOCO"/* | grep "text/x-shellscript"  | cut -d ":" -f 1`
-    echo "Script shell"
+    echo -e "\033[0;33mScript shell"
 elif [[ $(file --mime-type "$PATHGIOCO"/* | grep "application/x-executable") ]]
     then
-    echo "Eseguibile binario"
+    echo -e "\033[0;34mEseguibile binario"
     if [[ $(file --mime-type "$PATHGIOCO"/* | grep "application/x-executable" | wc -l) > 1 ]]
     then
         ESEGUIBILE=`file --mime-type "$PATHGIOCO"/* | grep "application/x-executable" | grep x86_64 | cut -d ":" -f 1`
-        echo "Eseguibile x86"
+        echo -e "\033[0;35mEseguibile x86"
     elif [[ $(file --mime-type "$PATHGIOCO"/* | grep "application/x-executable" | wc -l) = 1 ]]
     then
         ESEGUIBILE=`file --mime-type "$PATHGIOCO"/* | grep "application/x-executable" | cut -d ":" -f 1`
-        echo "Eseguibile generico"
-	sleep 3
+        echo -e "\033[0;36mEseguibile generico"
     fi
 fi
 
-echo $ESEGUIBILE
 
-echo "Setto il permesso +x all'eseguibile"
-chmod 755 "$ESEGUIBILE"
+echo -e "\033[0;32mSetto il permesso +x all'eseguibile\033[0;36m"
+chmod -v 755 "$ESEGUIBILE"
 
-echo "Scarico l'icona del gioco"
-echo $NOMEICONA
+echo -e "\033[0;32mScarico l'icona del gioco: \033[0;37m$NOMEICONA"
 NOMERICERCA=`echo $NOMEICONA | sed -e 's/\ /+/g'`
 URL=$(curl --silent https://itch.io/search?q=$NOMERICERCA | awk -F "data-lazy_src" '{print $2}' | awk -F "\"" '{print $2}' | grep https)
 if [ -v $URL ];
 then
-        echo "nessuna immagine trovata"
+        echo -e "\033[0;31mnessuna immagine trovata"
 	NOMEFILE="noimage.png"
 else
-        echo "Scarico il file"
+        echo -e "\033[0;32mScarico il file dell'icona"
         NOMEFILE=$(basename $URL)
         curl -o data/$NOMEFILE $URL
 fi
 
 
-echo "Creo il collegamento"
+echo -e "\033[0;32mCreo il collegamento\033[0;37m"
 echo "$NOMEICONA,\"$ESEGUIBILE\",$NOMEFILE" >> data/lista.csv
 echo "[Desktop Entry]" > "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Version=1.0" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
@@ -97,12 +93,12 @@ echo "Type=Application" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Name=$NOMEICONA"  >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Comment=$NOMEICONA" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Exec=\"$HOME/itchbox/$ESEGUIBILE\"" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
-echo "Icon=$HOME/itchbox/data/itchbox128.png" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
+echo "Icon=$HOME/itchbox/data/$NOMEFILE" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Path=" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "Terminal=false" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "StartupNotify=false" >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 echo "GenericName=$NOMEDIR"  >> "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
 
 
-echo "Setto il permesso +x al collegamento"
-chmod 755 "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
+echo -e "\033[0;32mSetto il permesso +x al collegamento\033[0;36m"
+chmod -v 755 "$HOME/$DESKTOPPATH/$NOMEDIR.desktop"
--- install-itchbox.sh
+++ install-itchbox.sh
@@ -4,10 +4,10 @@ clear
 echo -e "\t\033[0;31m*-----------> Installo il core system di itchbox...\033[0;36m"
 cd
 echo -e "\t\033[0;31m**---------->Creo le cartelle di sistema...\033[0;36m"
-mkdir $HOME/itchbox
-mkdir $HOME/itchbox/data
-mkdir $HOME/itchbox/download
-mkdir $HOME/itchbox/games
+mkdir -v $HOME/itchbox
+mkdir -v $HOME/itchbox/data
+mkdir -v $HOME/itchbox/download
+mkdir -v $HOME/itchbox/games
 echo -e "\t\033[0;31m***---------> Scarico e installo curl...\033[0;36m"
 sudo apt -y install curl
 echo -e "\t\033[0;31m****--------> Sarico i sorgenti di joymap...\033[0;36m"
@@ -20,28 +20,27 @@ gcc joymap.c -lxdo -o itchbox/data/joymap
 rm joymap.c
 echo -e "\t\033[0;31m*******------> Scarico le altre parti del core system...\033[0;36m"
 curl --output $HOME/itchbox/estrai.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/estrai.sh
-curl --output $HOME/itchbox/spegni.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main//spegni.sh
 curl --output $HOME/itchbox/data/sfondo.jpg --url https://raw.githubusercontent.com/carlominucci/itchbox/main/sfondo.jpg
 curl --output $HOME/itchbox/data/itchbox128.png --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox128.png
 curl --output $HOME/itchbox/joypadconf.sh --url https://raw.githubusercontent.com/carlominucci/itchbox/main/joypadconf.sh
-chmod 775 $HOME/itchbox/joypadconf.sh
+chmod -v 775 $HOME/itchbox/joypadconf.sh
 echo -e "\t\033[0;31m********-----> Setto i permessi del core system...\033[0;36m"
-chmod 775 $HOME/itchbox/estrai.sh
-chmod 775 $HOME/itchbox/spegni.sh
+chmod -v 775 $HOME/itchbox/estrai.sh
 echo -e "\t\033[0;31m*********----> Scarico e installo i comandi del core system...\033[0;36m"
 curl --output $HOME/$DESKTOPPATH/Aggiorna.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Aggiorna.desktop 
 curl --output $HOME/$DESKTOPPATH/Spegni.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/Spegni.desktop
-chmod 775 $HOME/$DESKTOPPATH/Aggiorna.desktop
-chmod 775 $HOME/$DESKTOPPATH/Spegni.desktop
+chmod -v 775 $HOME/$DESKTOPPATH/Aggiorna.desktop
+chmod -v 775 $HOME/$DESKTOPPATH/Spegni.desktop
 echo -e "\t\033[0;31m**********---> Configuro l'avvio automatico di itchbox...\033[0;36m"
-mkdir $HOME/.config/autostart/
+mkdir -v $HOME/.config/autostart/
 curl --output $HOME/.config/autostart/itchbox.desktop --url https://raw.githubusercontent.com/carlominucci/itchbox/main/itchbox.desktop
-chmod 755 $HOME/./config/autostart/itchbox.desktop
+chmod -v 755 $HOME/.config/autostart/itchbox.desktop
 echo -e "\t\033[0;31m***********--> Configuro la condivisione di rete per l'upload dei pacchetti dei giochi...\033[0;36m"
 sudo apt -y install samba
 sudo apt -y install samba-common
-sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
+sudo mv -v /etc/samba/smb.conf /etc/samba/smb.conf.orig
 sudo curl --output /etc/samba/smb.conf --url https://raw.githubusercontent.com/carlominucci/itchbox/main/smb.conf
 echo -e "\t\033[0;31m************-> Inserisci la password per la cartella di rete...\033[0;36m"
+sudo smbpasswd -a itchbox
 sudo /etc/init.d/smbd restart
 echo -e "\t\033[0;32m*************> Installazione completata...\033[0;36m"
