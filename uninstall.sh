081b2f89eec03bf353f322829ed11930fc60031a
Carlo Minucci <gecco@napodano.com>
2023-12-11 13:16:58 +0100

    sistemati alcuni problemetti allo script di disinstallazione dei giochi

--- uninstall.sh
+++ uninstall.sh
@@ -2,23 +2,29 @@
 clear
 if [[ -z $1 ]]
 then
-	echo "Lista giochi installati:"
+	echo -e "\033[0;31mLista giochi installati:\033[0;36m"
 	echo 
-	cat $HOME/itchbox/data/lista.csv | awk -F "," '{print $1}'
+	cat $HOME/itchbox/data/lista.csv | grep -v "Aggiorna" | grep -v "Spegni" | awk -F "," '{print $1}'
 	exit 1
 fi
-echo "Disinstallo il gioco: $1"
+if [[ -z `cat data/lista.csv | grep "$1"` ]]
+then
+	echo "Nessun gioco installato con questo nome"
+	exit 1
+fi
+
+echo -e "\033[0;31mDisinstallo il gioco:\033[0;36m $1"
 DESKTOPPATH=$(cat $HOME/.config/user-dirs.dirs | grep DESKTOP | awk -F "/" '{print $2}' | sed -e 's/"//g')
-PATHGAME=`cat data/lista.csv | grep "$1" | awk -F "\"" '{print $2}' | head -1 | awk -F "/" '{print $1 "/" $2}'`
-NOMEICONA=`basename "$PATHGAME"`
-NOME=`basename "$PATHGAME" | awk -F "." '{print $1}'`
+PATHGAME=`cat data/lista.csv | grep -i "$1" | head -1 | awk -F "," '{print $2}' | awk -F "/" '{print $1"/"$2}'`
+NOMEICONA=`echo "$PATHGAME" | awk -F "/" '{print $2}'`
+echo $1
+echo $NOMEICONA
 echo $PATHGAME
 echo $NOMEICONA
-echo $NOME
 
-echo "Rimuovo la directory del gioco"
+echo -e "\033[0;31mRimuovo la directory del gioco\033[0;36m"
 rm -vrf "$PATHGAME"
-echo "Rimuovo il collegamento"
+echo -e "\033[0;31mRimuovo il collegamento\033[0;36m"
 rm -v "$HOME/$DESKTOPPATH/$NOMEICONA.desktop"
-echo "Rimuovo il riferimento nel registro"
+echo -e "\033[0;31mRimuovo il riferimento nel registro\033[0;36m"
 sed -i "/$1/d" $HOME/itchbox/data/lista.csv 
